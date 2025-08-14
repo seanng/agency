4d54bf780a3e2d18ca7aa5e270c1ba3ba827e7b6
sean <shonum@gmail.com>
2025-08-14 13:05:37 +0800

    do not show wanyi web

--- apps/drafts/serve.ts
+++ apps/drafts/serve.ts
@@ -1,10 +1,11 @@
+// @ts-nocheck
 import { serve } from 'https://deno.land/std@0.224.0/http/server.ts';
 import { serveDir } from 'https://deno.land/std@0.224.0/http/file_server.ts';
 
 serve((req) => {
   return serveDir(req, {
-    fsRoot: '.',
-    showDirListing: true,
+    fsRoot: 'apps/drafts/projects',
+    showDirListing: false,
     enableCors: true,
   });
 });
